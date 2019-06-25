c9475a3bb44e63870f27a2109e80b6ce122f35b9
drobik16 <51448323+drobik16@users.noreply.github.com>
2019-06-25 11:22:24 +0300

    update

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var randomkey = null;
 var totalbonus = 0;
 var TimerId = null;
 var IntervalId = null;
+var count=0;
+var timer;
 
 document.body.appendChild(document.createElement("BR"));
 
@@ -12,7 +14,7 @@ obj.className = "key_block";
 document.body.appendChild(document.createElement("BR"));
 document.body.appendChild(document.createElement("BR"));
 
-var key_array=    ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"," "];
+var key_array= 		["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"," "];
 var key_code_array= [ 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86, 66, 78, 77];
 var key_obj = [];
 
@@ -46,23 +48,32 @@ function myfunction(event) {
     not_correct_key();
   }
 }
+  function timeCount() {
+    document.getElementById("countTime").innerHTML = count.toString();
+    count++;
+    timer = window.setTimeout(function(){ timeCount() },1000);
+  }
+   function startCount() {
+    if (!timer)
+      timeCount();
+  }
+  function stopCount() {
+      if (timer) {
+        clearTimeout(timer);
+        timer=null;
+      }
+    }
 function start() {
   if (randomkey === null) {
     randomkey = Math.floor(Math.random()*(key_array.length - 1));
     key_obj[randomkey].style.color = "white";
-    key_obj[randomkey].style.backgroundColor = "black"; 
+    key_obj[randomkey].style.backgroundColor = "black";	
     obj.innerHTML = key_array[randomkey];
     key_obj[26].innerHTML = 3;
-    IntervalId = setInterval(printdate_s,1000);
-    TimerId = setTimeout(time_over,3000); 
+   	IntervalId = setInterval(printdate_s,1000);
+   	TimerId = setTimeout(time_over,3000);	
   }
-}
-function pause_key() {
-  clearTimeout(TimerId);
-  clearInterval(IntervalId);
-  key_obj[26].innerHTML = "P";
-  inform_obj.innerHTML = 'Пауза';
-}   
+}	
 function printdate_s() {
   key_obj[26].innerHTML--;
 }
@@ -73,10 +84,10 @@ function correct_key() {
   inform_obj.innerHTML = "Правильно + 1 бал. Итого " + totalbonus + " балов!";
   inform_obj.style.color = "green";
   key_obj[randomkey].style.color = "black";
-  key_obj[randomkey].style.backgroundColor = "white"; 
-  obj.innerHTML = ""; 
+  key_obj[randomkey].style.backgroundColor = "white";	
+  obj.innerHTML = "";	
   randomkey = null;
-  start();  
+  start();	
 }
 function not_correct_key() {
   clearTimeout(TimerId);
@@ -86,11 +97,11 @@ function not_correct_key() {
   inform_obj.style.color = "red";
   if (randomkey != null) {
     key_obj[randomkey].style.color = "black";
-    key_obj[randomkey].style.backgroundColor = "white"; 
-    obj.innerHTML = ""; 
+    key_obj[randomkey].style.backgroundColor = "white";	
+    obj.innerHTML = "";	
   }
   randomkey = null;
-  start();  
+  start();	
 }
 function space_key() {
   clearTimeout(TimerId);
@@ -99,12 +110,13 @@ function space_key() {
   inform_obj.style.color = "black";
   if (randomkey != null) {
     key_obj[randomkey].style.color = "black";
-    key_obj[randomkey].style.backgroundColor = "white"; 
-    obj.innerHTML = ""; 
+    key_obj[randomkey].style.backgroundColor = "white";	
+    obj.innerHTML = "";	
   }
   totalbonus = 0;
   randomkey = null;
-  start();  
+  timer1.start()
+  start();	
 }
 function time_over() {
   clearTimeout(TimerId);
@@ -114,9 +126,88 @@ function time_over() {
   inform_obj.style.color = "red";
   if (randomkey != null) {
     key_obj[randomkey].style.color = "black";
-    key_obj[randomkey].style.backgroundColor = "white"; 
-    obj.innerHTML = ""; 
+    key_obj[randomkey].style.backgroundColor = "white";	
+    obj.innerHTML = "";	
   }
   randomkey = null;
-  start();  
-} 
+  start();	
+}
+
+var time = 60,
+    fps = 60;
+
+var Timer = function(obj){
+  this.time = obj.time;
+  this.fps = obj.fps;
+  this.onEnd = obj.onEnd || null;
+  this.onStart = obj.onStart || null;
+  this.onTick = obj.onTick || null;
+  this.intervalID = null;
+
+  this.start = () => {
+    this.interval = setInterval(this.update, 1000 / this.fps);
+    this.onStart ? this.onStart() : void 0;
+    return this;
+  };
+  this.stop = () => {
+    clearInterval(this.interval);
+    this.onEnd ? this.onEnd() : void 0;
+  };
+  this.update = () => {
+    this.time > 0 ? this.time -= 1/this.fps : this.stop();
+    this.onTick ? this.onTick() : void 0;
+    return this.get();
+  }
+  this.get = (par) => {
+    switch(par) {
+      case undefined:
+        return this.time;
+        break;
+      case "dig":
+        return Math.ceil(this.time);
+        break;
+      case "end":
+        return this.onEnd();
+        break;
+    }
+  }
+}
+
+var timer1 = new Timer({
+  time: time,
+  fps: fps,
+  onTick: tick,
+  onEnd: endTimer,
+  onStart: onTimerStart
+});
+
+function onTimerStart(){
+  console.log("timer started");
+}
+function endTimer(){
+  console.log("timer ended");
+  endGame();
+}
+
+
+requestAnimationFrame(tick);
+function tick(){
+  id("output").innerHTML = timer1.get("dig");
+  id("slider").style.width = timer1.get()/time *100 + "%";
+}
+
+function id(id){
+  return document.getElementById(id);
+}
+
+
+function endGame(){
+	alert('Ваш результат ' + totalbonus);
+  	randomkey = null;
+ 	totalbonus = 0;
+ 	clearTimeout(TimerId);
+  	clearInterval(IntervalId);
+ 	count=0;
+ 	timer=0;
+}
+
